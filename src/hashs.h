3b862706cbf61138cbd6e1e357bc38a38ae8c4b2
dh <46441813+DeanHnter@users.noreply.github.com>
2024-07-30 16:32:56 +0200

    Update hashs.h

--- src/hashs.h
+++ src/hashs.h
@@ -13,9 +13,12 @@ void blake2b(uint8_t       *hash   , size_t hash_size,
 #define BASE58_ENCODE_MAXLEN 256
 #define BASE58_DECODE_MAXLEN 360
 
-bool base58_decode(char *bin, size_t *binszp, const char *b58, size_t b58sz);
-bool base58_encode(char *b58, size_t *b58sz, const char *data, size_t binsz);
+bool base58_decode(uint8_t *bin, size_t *binszp, const char *b58, size_t b58sz);
+bool base58_encode(char *b58, size_t *b58sz, const uint8_t *data, size_t binsz);
 bool b58tobin(char *bin, size_t *binszp, const char *b58, size_t b58sz);
+
+
+
 
 #endif
 
